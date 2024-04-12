f7e36e4b36cf34d91a8068deb1484fd72478ef54
AnnaHornman <anna@hornman.com>
2024-04-12 11:46:20 +0200

    prettygoat4

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main():
         filepath= 'C:/Users/EmmaS/Documents/M7-Python/Final Project/data/for_recognition/9cdb7a9b-afc5-4541-a736-1997efa5842a.jpg'
         sensor_data_path = "C:/Users/EmmaS/Documents/M7-Python/Final Project/csv_files/PlantDataLabels.csv"
     if user == 'Anna':
-        filepath=''
-        sensor_data_path=''
+        filepath='C:/Users/anna/PycharmProjects/AquaFlora-Forecast/data/for_recognition/9cdb7a9b-afc5-4541-a736-1997efa5842a.jpg'
+        sensor_data_path='C:/Users/anna/PycharmProjects/AquaFlora-Forecast/csv_files/PlantDataLabels.csv'
 
     com_port = "COM17"
     baud_rate = 9600
