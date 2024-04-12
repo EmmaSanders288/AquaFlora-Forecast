e58ae55c697952c171d1fe006d2f24e04812a35c
Emma-Sanders <326358@annavanrijn.nl>
2024-04-12 11:44:03 +0200

    prettygoat3

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from SVM_model import PlantWaterNeedPredictor
 # from saveDataToExcel import SensorDataLogger
 
 def main():
-    filepath= 'C:/Users/EmmaS/Documents/M7-Python/Final Project/data/for_recognition/9cdb7a9b-afc5-4541-a736-1997efa5842a.jpg'
-    sensor_data_path = "C:/Users/EmmaS/Documents/M7-Python/Final Project/csv_files/PlantDataLabels.csv"
+    user = ''
+    if user == 'Emma':
+        filepath= 'C:/Users/EmmaS/Documents/M7-Python/Final Project/data/for_recognition/9cdb7a9b-afc5-4541-a736-1997efa5842a.jpg'
+        sensor_data_path = "C:/Users/EmmaS/Documents/M7-Python/Final Project/csv_files/PlantDataLabels.csv"
+    if user == 'Anna':
+        filepath=''
+        sensor_data_path=''
+
     com_port = "COM17"
     baud_rate = 9600
 
